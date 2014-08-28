dedb93b926b73af9bd3bb6be370e480a90df487f
Geneva-Ana <anastasiya.mishchuk@gmail.com>
2014-08-29 02:04:25 +0300

    removed regex and added library code for multiple strings

--- nlpTravelPlannerReviewAnalyzer/src/main/java/com/codeminders/labs/timeextractor/service/GetHtmlText.java
+++ nlpTravelPlannerReviewAnalyzer/src/main/java/com/codeminders/labs/timeextractor/service/GetHtmlText.java
@@ -3,8 +3,6 @@ package com.codeminders.labs.timeextractor.service;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 import org.jsoup.Jsoup;
@@ -17,47 +15,59 @@ import com.codeminders.labs.timeextractor.entities.HtmlElement;
 
 public class GetHtmlText {
 
-    public ArrayList<HtmlElement> getElements(String html) {
+	public ArrayList<HtmlElement> getElements(String html) {
 
-        ArrayList<HtmlElement> elements = new ArrayList<HtmlElement>();
-        Document document = Jsoup.parse(html);
-        Elements htmlElements = document.body().select("*");
-        for (Element element : htmlElements) {
-            if (element.ownText().isEmpty() || element.ownText().length() < 2 || element.toString().length() > 10000) {
-                continue;
-            }
-            String text = element.ownText();
-            System.out.println("element: " + element);
-            try {
-                String elementString = text.replace("(", "\\(").replace(")", "\\)").replace("{", "\\{").replace("?", "\\?").replace("}", "\\}").replace(".", "\\.").replace("|", "\\|")
-                        .replace("*", "\\*").replaceAll(text, "\\s*(" + text + ")\\s*");
-                System.out.println(elementString);
-                Pattern p = Pattern.compile(elementString);
-                Matcher m = p.matcher(html);
-                while (m.find()) {
-                    HtmlElement htmlEl = new HtmlElement();
-                    htmlEl.setTag(element.tagName());
-                    htmlEl.setTextFrom(m.start());
-                    htmlEl.setTextTo(m.end());
-                    htmlEl.setText(text);
-                    elements.add(htmlEl);
-                }
-            } catch (PatternSyntaxException ex) {
-                System.out.println(ex);
-            }
-        }
+		ArrayList<HtmlElement> elements = new ArrayList<HtmlElement>();
+		Document document = Jsoup.parse(html);
+		document.outputSettings(new Document.OutputSettings()
+				.prettyPrint(false));
+		Elements htmlElements = document.body().select("*");
+		for (Element element : htmlElements) {
+			if (element.childNodes().size() > 1) {
+				continue;
+			}
+			if (element.ownText().isEmpty() || element.ownText().length() < 2
+					|| element.toString().length() > 10000) {
+				continue;
+			}
+			String text = element.ownText();
+			try {
+				String elementString = element.toString();
 
-        return elements;
-    }
+				if (!html.contains(elementString)) {
+					System.out.println("Element :" + element);
+					System.out.println("Text :" + text);
 
-    public static void main(String[] args) throws IOException {
-        Document document = Jsoup.connect("http://www.euronews.com/news/europe/").get();
-        String htmlString = document.toString();
-        SUTimeService service = new SUTimeService();
-        List<AnnotationIntervalHtml> extractedDatesAndTimes = service.extractDatesAndTimeFromHtml(htmlString);
-        System.out.println(extractedDatesAndTimes.size());
+				}
+				if (html.contains(elementString)) {
 
-        System.out.println(extractedDatesAndTimes);
+					HtmlElement htmlEl = new HtmlElement();
+					htmlEl.setTag(element.tagName());
+					htmlEl.setTextFrom(html.indexOf(element.toString()));
+					htmlEl.setTextTo(html.lastIndexOf(element.toString()));
+					htmlEl.setText(text);
+					elements.add(htmlEl);
+				}
 
-    }
+			} catch (PatternSyntaxException ex) {
+				System.out.println("Exception: " + ex);
+			}
+		}
+
+		return elements;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		Document document = Jsoup.connect(
+				"http://www.euronews.com/news/europe/").get();
+		String htmlString = document.toString();
+		SUTimeService service = new SUTimeService();
+		List<AnnotationIntervalHtml> extractedDatesAndTimes = service
+				.extractDatesAndTimeFromHtml(htmlString);
+		System.out.println(extractedDatesAndTimes.size());
+
+		System.out.println(extractedDatesAndTimes);
+
+	}
 }

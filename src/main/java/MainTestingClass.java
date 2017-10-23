5427d5623756d7ea00210e8c5e5bcab033b2c3eb
annKhan <hannakhanko@gmail.com>
2017-10-23 14:21:18 +0300

    changed relative path to training data

--- src/main/java/MainTestingClass.java
+++ src/main/java/MainTestingClass.java
@@ -12,12 +12,10 @@ import ai.digamma.service.TimeExtractor;
 import ai.digamma.utils.SettingsBuilder;
 
 public class MainTestingClass {
-    private static String TRAINING_DATA = "/home/anna/time/timeextractor/data/train.csv";
-    private static String TEST_RESULTS_FILE = "/home/anna/time/timeextractor/data/results.txt";
+    private static String TRAINING_DATA = System.getProperty("user.dir") + "/data/train.csv";
+    private static String TEST_RESULTS_FILE = System.getProperty("user.dir") + "/data/results.txt";
 
     public static void main(String[] args) throws Exception {
-
-        TemporalExtractionService service = new TemporalExtractionService();
         CsvReader reader = new CsvReader();
         String csv = TRAINING_DATA;
         CsvWriter writer = new CsvWriter();

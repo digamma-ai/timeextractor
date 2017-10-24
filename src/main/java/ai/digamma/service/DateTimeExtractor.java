b13e1230f9798cfd613f5b3688be91db3a39c2bc
annKhan <hannakhanko@gmail.com>
2017-10-24 17:18:18 +0300

    added exception handling for reading csv file

--- src/main/java/ai/digamma/service/DateTimeExtractor.java
+++ src/main/java/ai/digamma/service/DateTimeExtractor.java
@@ -5,7 +5,9 @@ import ai.digamma.business.CsvWriter;
 import ai.digamma.entities.Settings;
 import ai.digamma.entities.TemporalExtraction;
 import ai.digamma.entities.Tip;
+import ai.digamma.exceptions.ExceptionMessages;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -24,12 +26,17 @@ public class DateTimeExtractor {
         return extracted;
     }
 
-    public static TreeSet<TemporalExtraction> extractFromCsv(String csvPath, String separator, String outputPath, Settings settings) throws Exception{
+    public static TreeSet<TemporalExtraction> extractFromCsv(String csvPath, String separator, String outputPath, Settings settings)
+            throws Exception {
         CsvReader reader = new CsvReader();
         CsvWriter writer = new CsvWriter();
+        List<Tip> tips;
         TemporalExtractionService service = new TemporalExtractionService();
         TreeSet<TemporalExtraction> extracted = new TreeSet<>();
-        List<Tip> tips = reader.getTipsFromFile(csvPath, separator);
+        try {
+            tips = reader.getTipsFromFile(csvPath, separator);
+        }
+        catch(IOException e){ throw new Exception(ExceptionMessages.INPUT_FILE_NOT_FOUND);}
         for (Tip tip : tips) {
             String text = tip.getTipText();
             TreeSet<TemporalExtraction> curr_extracted = service.extractDatesAndTimeFromText(text,settings);

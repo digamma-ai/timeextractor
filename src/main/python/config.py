049d7229b7d1fb4ec530214c599541816de7a5e0
karim.lulu <karimlulu@karim.lulu>
2017-10-24 20:10:54 +0300

    added python wrappers for temporal extractions

--- src/main/python/config.py
+++ src/main/python/config.py
@@ -5,3 +5,5 @@ project_dir = os.path.join(python_src_dir, os.pardir, os.pardir, os.pardir)
 
 JAR_FILENAME = "timeextractor-jar-with-dependencies.jar"
 PATH_TO_JAR = os.path.join(project_dir, "target", JAR_FILENAME)
+
+JavaSettingsConstructorParams = ['date', 'timezoneOffset', 'rulesToIgnore', 'rulesToInclude', 'includeOnlyLatestDates']

4efbebdebf4b6d00bb22f269bb6f041f1bd7f5ef
annKhan <hannakhanko@gmail.com>
2017-10-24 17:13:15 +0300

    added reading from csv files

--- src/main/java/ai/digamma/service/DateTimeExtractor.java
+++ src/main/java/ai/digamma/service/DateTimeExtractor.java
@@ -1,8 +1,12 @@
 package ai.digamma.service;
 
+import ai.digamma.business.CsvReader;
+import ai.digamma.business.CsvWriter;
 import ai.digamma.entities.Settings;
 import ai.digamma.entities.TemporalExtraction;
+import ai.digamma.entities.Tip;
 
+import java.util.List;
 import java.util.TreeSet;
 
 public class DateTimeExtractor {
@@ -20,4 +24,20 @@ public class DateTimeExtractor {
         return extracted;
     }
 
+    public static TreeSet<TemporalExtraction> extractFromCsv(String csvPath, String separator, String outputPath, Settings settings) throws Exception{
+        CsvReader reader = new CsvReader();
+        CsvWriter writer = new CsvWriter();
+        TemporalExtractionService service = new TemporalExtractionService();
+        TreeSet<TemporalExtraction> extracted = new TreeSet<>();
+        List<Tip> tips = reader.getTipsFromFile(csvPath, separator);
+        for (Tip tip : tips) {
+            String text = tip.getTipText();
+            TreeSet<TemporalExtraction> curr_extracted = service.extractDatesAndTimeFromText(text,settings);
+            for(TemporalExtraction temp : curr_extracted) {
+                extracted.add(temp);
+                writer.writeToFile(outputPath, extracted.toString());
+            }
+        }
+        return extracted;
+    }
 }
